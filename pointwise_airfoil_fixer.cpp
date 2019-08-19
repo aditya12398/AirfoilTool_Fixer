3d111fddf6c211170abe7d8b379075abde5072b2
aditya12398 <aditya12398@gmail.com>
2019-08-19 20:13:28 +0530

    Commented out using namespace std;

--- pointwise_airfoil_fixer.cpp
+++ pointwise_airfoil_fixer.cpp
@@ -4,16 +4,16 @@
 #include <cmath>
 
 double x, y, z = 0;
-using namespace std;
+//using namespace std;
 
-double trailing_edge_point(string name, int line)
+double trailing_edge_point(std::string name, int line)
 {
     double a1, a2, b1, b2, c1, c2;
     double x_trailing, y_trailing;
     double x_old, y_old, x_new, y_new;
     int count = 1;
     x_old = y_old = -10000;
-    ifstream infile(name);
+    std::ifstream infile(name);
     for (int i = 1; i <= line; i++)
     {
         infile >> x >> y;
@@ -38,8 +38,8 @@ double trailing_edge_point(string name, int line)
         count++;
     }
     infile.close();
-    ifstream infile2(name);
-    ofstream outfile("pw_origin_" + name);
+    std::ifstream infile2(name);
+    std::ofstream outfile("pw_origin_" + name);
     outfile.precision(20);
     a2 = (y_old - y_new);
     b2 = (x_new - x_old);
@@ -51,7 +51,7 @@ double trailing_edge_point(string name, int line)
         x_trailing = 0;
     else if (fabs(y_trailing) <= 1E-12)
         y_trailing = 0;
-    outfile << line + 2 << endl;
+    outfile << line + 2 << std::endl;
     outfile << x_trailing << "\t" << y_trailing << "\t" << z << "\n";
     for (int i = 1; i <= line; i++)
     {
@@ -70,26 +70,26 @@ int main()
     double tr_x = 0, tr_y = 0, scale_factor = 1.0, chord_length = 1.0;
     double x_old, y_old, x_new, y_new, length;
     x_old = y_old = -10000;
-    string name, outname;
+    std::string name, outname;
 
-    cout << "Enter the filename with extension: ";
-    cin >> name;
-    ifstream infile(name);
-    cout << "Do you want to change the chord length of the airfoil(0/1)? ";
-    cin >> scale_flag;
+    std::cout << "Enter the filename with extension: ";
+    std::cin >> name;
+    std::ifstream infile(name);
+    std::cout << "Do you want to change the chord length of the airfoil(0/1)? ";
+    std::cin >> scale_flag;
     if (scale_flag != 0)
     {
-        cout << "Enter the chord length: ";
-        cin >> chord_length;
+        std::cout << "Enter the chord length: ";
+        std::cin >> chord_length;
     }
-    cout << "Do you want to translate the geometry from origin(0/1)? ";
-    cin >> translate_flag;
+    std::cout << "Do you want to translate the geometry from origin(0/1)? ";
+    std::cin >> translate_flag;
     if (translate_flag != 0)
     {
-        cout << "Enter x translate value: ";
-        cin >> tr_x;
-        cout << "Enter y translate value: ";
-        cin >> tr_y;
+        std::cout << "Enter x translate value: ";
+        std::cin >> tr_x;
+        std::cout << "Enter y translate value: ";
+        std::cin >> tr_y;
     }
     while (!infile.eof())
     {
@@ -111,18 +111,18 @@ int main()
         length = trailing_edge_point(name, line);
     else
         length = sqrt(x_new * x_new + y_new * y_new);
-    cout.precision(20);
+    std::cout.precision(20);
     scale_factor = chord_length / length;
-    cout << length << "\t" << scale_factor << endl;
-    ifstream infile2("pw_origin_" + name);
-    ofstream outfile("scaled_pw_" + name);
+    std::cout << length << "\t" << scale_factor << std::endl;
+    std::ifstream infile2("pw_origin_" + name);
+    std::ofstream outfile("scaled_pw_" + name);
     outfile.precision(20);
     infile2 >> line;
-    outfile << line << endl;
+    outfile << line << std::endl;
     for (int i = 1; i <= line; i++)
     {
         infile2 >> x >> y >> z;
-        outfile << (x * scale_factor + tr_x) << "\t" << (y * scale_factor + tr_y) << "\t" << (z) << endl;
+        outfile << (x * scale_factor + tr_x) << "\t" << (y * scale_factor + tr_y) << "\t" << (z) << std::endl;
     }
     infile2.close();
     outfile.close();
